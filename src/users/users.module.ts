71ca3675624ce8d2c971936584e57ec17c676371
Maximiliano Sorribas <mjsorribas@gmail.com>
2025-10-28 13:19:32 -0300

    feat(users): add users and roles entities

---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { Role } from '../roles/entities/role.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Role])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService],
+})
+export class UsersModule {}
